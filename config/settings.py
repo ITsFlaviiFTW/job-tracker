131e489d164d11a850a619d893c24014bd3308b1
Flavius Porumbiel <Fporumbiel8739@conestogac.on.ca>
2025-09-09 20:43:28 -0400

    changed settings.py

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,8 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent      # /opt/jobtracker/config
+PROJECT_ROOT = BASE_DIR.parent
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = config('MEDIA_ROOT', default=str(BASE_DIR / 'media'))
@@ -135,9 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [ BASE_DIR / 'jobtracker' / 'static' ]
+STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/jobtracker/config/staticfiles'
+STATICFILES_DIRS = [ PROJECT_ROOT / 'jobtracker' / 'static' ]
 
 
 STATICFILES_STORAGE = (
